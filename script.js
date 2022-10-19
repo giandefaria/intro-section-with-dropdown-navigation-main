b4d229d29702c5b8c381889bd55093712a6668a5
Gian <gian.defaria@hotmail.com>
2022-10-18 23:52:04 -0300

    Adicionei uma busca de ID do dropDown acionado. Com isso, posso manipular o display da caixa que será aberta juntamente com os arrows.

--- script.js
+++ script.js
@@ -17,17 +17,21 @@ for (let i = 0; i < selectedMenu.length; i++) {
     selectedMenu[i].addEventListener('click', function (e) {
         let IdArrowUp = selectedMenu[i].lastChild.lastChild.id;
         let IdArrowDown = selectedMenu[i].firstChild.firstElementChild.id;
-        
+        let event = e.path[1].nextElementSibling.id;
+                
         const arrowDown = document.getElementById(IdArrowDown);
         const arrowUp = document.getElementById(IdArrowUp);
-        console.log(arrowDown);
+        const dropDown = document.getElementById(event);
+        console.log(dropDown);
 
         if (arrowUp.style.display == 'none') {
             arrowDown.style.display = 'none';
             arrowUp.style.display = 'inline-block';
+            dropDown.style.display = 'block';
             } else {
             arrowUp.style.display = 'none';
             arrowDown.style.display = 'inline-block';
+            dropDown.style.display = 'none';
             }    
         }
     )
